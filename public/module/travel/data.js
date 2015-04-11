2b0be816bfc23c412dcdcde70a539ecfbae7aaa2
Alex Wang <lwang@freewheel.tv>
2015-04-11 14:29:20 +0800

    new page

--- public/module/travel/data.js
+++ public/module/travel/data.js
@@ -148,6 +148,15 @@ sight = [
         description: 'amazing Fall'
       }
     },
+    extra: [
+      {
+        title: 'Empress Dowsdf CiXi',
+        content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably'
+      }, {
+        title: 'Tai hou',
+        content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably'
+      }
+    ],
     sub: [
       {
         title: 'RenShoudian',
